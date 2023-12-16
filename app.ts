0a57977fd1aba683b49887425b9757e494db0241
VillegasMich <manuel.villegas.michel@gmail.com>
2023-12-15 19:50:49 -0500

    CORS error solved

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
 import { Elysia, t } from 'elysia';
 import { ingredientRouter } from './src/components/routes/ingredient';
 import { dishRouter } from './src/components/routes/dish';
-import cors from '@elysiajs/cors';
+import { cors } from '@elysiajs/cors';
 
 const app = new Elysia()
+  .use(cors())
   .group('/ingredients', (app) => app.use(ingredientRouter))
   .group('/dishes', (app) => app.use(dishRouter))
-  .use(cors())
   .listen(8080);
 
 console.log(
